df783acd8710fc2a9e23df1890cb057374d72cdf
CHIRANTH R RAICHUR <137791814+ChiranthRRaichur@users.noreply.github.com>
2024-06-13 23:16:22 +0530

    Update urls.py

--- django1/django1/urls.py
+++ django1/django1/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('app3/',include("app3.urls")),
     path('app4/',include("app4.urls")),
     path('app5/',include("app5.urls")),
+    path('app7/',include("app7.urls")),
+    path('app8/',include("app8.urls")),
 ]
